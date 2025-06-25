1435acb1582e4ce01fcfa4a90552b1233d9ba344
AndyTempel <9637751+AndyTempel@users.noreply.github.com>
2025-06-25 11:43:32 +0200

    Add initial project files and configuration for victron-dlms-reader

---
+++ version.py
@@ -0,0 +1,15 @@
+import logging
+import os
+import sys
+
+
+def VERSION():
+	try:
+		base = os.path.dirname(os.path.realpath(__file__))
+		with open(os.path.join(base, 'VERSION'), 'r') as version_file:
+			version = version_file.read().strip()
+			return version
+	except IOError as e:
+		logging.error("I/O error(%s): %s", e.errno, e.strerror)
+	except:  # handle other exceptions such as attribute errors  # noqa: E722
+		logging.error("Unexpected error: %s", sys.exc_info()[0])
